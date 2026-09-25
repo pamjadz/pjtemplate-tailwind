d461c6eb73c5a05472d865a6ddad55bc22a562a0
Pouria Amjadzadeh <p.amjadzadeh@gmail.com>
2026-09-25 16:23:27 +0330

    fixed offcanvas

--- components/offcanvas-menu.php
+++ components/offcanvas-menu.php
@@ -1,10 +1,10 @@
 <?php
 defined('ABSPATH') || exit;
 
-//Call This by <button class="btn" type="button" data-bs-toggle="offcanvas" data-bs-target="#mobileMenu" aria-controls="mobileMenu">MENU</button>
+//Call This by  <button type="button" data-pjstack="#mobileMenu" aria-haspopup="dialog" aria-controls="mobileMenu">Menu</button>
 ?>
 
-<div id="mobileMenu" class="offcanvas offcanvas-start" tabindex="-1" aria-labelledby="mobileMenuLabel">
+<div id="mobileMenu" class="offcanvas offcanvas-right" tabindex="-1" role="dialog" aria-labelledby="mobileMenuLabel" aria-hidden="true" aria-modal="true">
 	<div class="offcanvas-header">
 		<div id="mobileMenuLabel" class="offcanvas-title">Offcanvas</div>
 		<button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
